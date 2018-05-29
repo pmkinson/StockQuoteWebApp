70e294913eb0a5d1ac04050980b6dfc4a71af834
Peter Kinson <pmkinson@gmail.com>
2018-05-29 16:14:29 -0400

    Updated Licensing Header

--- src/main/java/com/uml/edu/stocksearch/servlet/StockSearchServlet.java
+++ src/main/java/com/uml/edu/stocksearch/servlet/StockSearchServlet.java
@@ -1,3 +1,24 @@
+/**
+ * Copyright 2018 Peter Kinson
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * package com.uml.edu.stocksearch.model;
+ * <p>
+ * /**
+ * Empty wrapper for all DAOObject objects.
+ */
+
 package com.uml.edu.stocksearch.servlet;
 
 import com.uml.edu.stocksearch.model.SearchDAO;
@@ -114,7 +135,7 @@ public class StockSearchServlet extends HttpServlet {
 
         }
 
-         /*
+        /*
          * Super Important!!!
          * Store formatted query in session data to be accessed by results page.
          * Nothing will be done to HTML formatted results after it is cast to final string.
@@ -151,8 +172,7 @@ public class StockSearchServlet extends HttpServlet {
      * @return boolean
      */
     @Override
-    public boolean equals(Object obj)
-    {
+    public boolean equals(Object obj) {
 
         return EqualsBuilder.reflectionEquals(this, obj);
     }
@@ -163,8 +183,7 @@ public class StockSearchServlet extends HttpServlet {
      * @return Object hashcode
      */
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
 
         return HashCodeBuilder.reflectionHashCode(this);
     }
